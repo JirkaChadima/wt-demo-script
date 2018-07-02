9e2a4c706a94268fc08767383b8a90c219891ecd
Jirka Chadima <chadima.jiri@gmail.com>
2018-07-02 12:34:09 +0200

    Brings demo script in sync with official developer guide

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// 1. Load the libs
 const WTLibs = require('@windingtree/wt-js-libs');
 const SwarmAdapter = require('@windingtree/off-chain-adapter-swarm');
 const HttpAdapter = require('@windingtree/off-chain-adapter-http');
 
+// 2. Configure the wt-js-libs
 const libs = WTLibs.createInstance({
   dataModelOptions: {
+    // We are using Ropsten as testnet, our demo index is on 0x407f550023eb6ad8a4797844489e17c5ced17e06
     provider: 'https://ropsten.infura.io/',
   },
   offChainDataOptions: {
     adapters: {
+      // WT is using bzz-raw protocol to access data on swarm
       'bzz-raw': {
         options: {
           swarmProviderUrl: 'https://swarm-gateways.net/',
@@ -16,6 +20,7 @@ const libs = WTLibs.createInstance({
           return new SwarmAdapter(options);
         },
       },
+      // WT is not using http at the moment, we only resolve https
       https: {
         create: () => {
           return new HttpAdapter();
@@ -25,26 +30,34 @@ const libs = WTLibs.createInstance({
   },
 });
 
+// 3. Collect the data
+// This has to be in an async block due to the nature of distributed data
 (async () => {
+  // Get an instance of WTIndex wrapper
   const index = await libs.getWTIndex('0x407f550023eb6ad8a4797844489e17c5ced17e06');
-  const hotels = await index.getAllHotels();
-  console.log('Checking an actual Winding Tree Index on Ropsten - 0x407f550023eb6ad8a4797844489e17c5ced17e06')
-  
-  console.log('='.repeat(80));
-  console.log('Checking out hotel with data on HTTPS')
-  console.log('hotel address: ' + await hotels[0].address);
-  const hotel1dataIndex = (await hotels[0].dataIndex);
-  console.log('off-chain data location: ' + await hotel1dataIndex.ref);
-  const hotel1descriptionData = await hotel1dataIndex.contents.descriptionUri;
-  console.log('off-chain description data location: ' + await hotel1descriptionData.ref);
-  console.log('off-chain stored hotel name: ' + await hotel1descriptionData.contents.name);
-  console.log('-'.repeat(80));
 
-  console.log('Checking out hotel with data on Swarm')
-  console.log('hotel address: ' + await hotels[1].address);
-  const hotel2dataIndex = (await hotels[1].dataIndex);
-  console.log('off-chain data location: ' + await hotel2dataIndex.ref);
-  const hotel2descriptionData = await hotel2dataIndex.contents.descriptionUri;
-  console.log('off-chain description data location: ' + await hotel2descriptionData.ref);
-  console.log('off-chain stored hotel name: ' + await hotel2descriptionData.contents.name);
+  // We can get all hotels available in the WTIndex
+  // - We will get only hotels on valid and accessible addresses
+  // - Every hotel is an abstraction over on-chain and off-chain data
+  // - This should be reasonably fast, because only a single method call on WTIndex is performed
+  const hotels = await index.getAllHotels();
+
+  // Notice how the approach is totally protocol agnostic
+  for (let hotel of hotels) {
+    // This actually fetches data from a hotel smart contract
+    const hotelDataUri = await hotel.dataUri;
+    // But it gets cached, so the second call is way faster
+    const hotelDataUri2 = await hotel.dataUri;
+
+    // Let's initialize the off-chain data index - but no data is downloaded yet
+    const offChainData = await hotel.dataIndex;
+    // Only the next command actually initiates the download of the data index document
+    const hotelDescriptionUri = await offChainData.contents.descriptionUri;
+    // And only now the actual contents of descriptionUri gets downloaded
+    const hotelName = await hotelDescriptionUri.contents.name;
+    // But this is fast, because the document is already cached in memory.
+    // The properties are of course interchangeable, so the data is downloaded
+    // only when any of the data properties is accessed for the first time.
+    const hotelDescription = await hotelDescriptionUri.contents.description;
+  }
 })();
